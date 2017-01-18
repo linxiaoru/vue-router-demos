2840b9a1e0df7b5394d4b7f30ebd70f20910e305
Cheryl Lin <linxiaoruxr@163.com>
2017-01-18 23:53:07 +0800

    修改一些内容，加入了路由说明

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="container">
-    <h2 class="text-center">Hello World</h2>
+  <div class="container text-center">
+    <h2>Hello World</h2>
+    
+    <ul class="box">
+    	<router-link to="/home">
+    		<p>点这也可以跳到 Home 页</p>
+    	</router-link>
+    	<router-link to="/about">
+    		<p>点这也可以跳到 About 页</p>
+    	</router-link>
+    	<router-link to="/more">
+    		<p>点这也可以跳到 More 页</p>
+    	</router-link>
+    </ul>
+
+    <div class="tips">
+    	这一页是由顶部的 Navbar 组件和下面的这个 Hello 组件组成，这两个组件分别插入在两个 router-view 中，
+    	Navbar 和上面的这个框中的链接都是用 router-link 标签写的，然后用 to 来指定链接的地址，而这个路由信息是要在 main.js 中配置的
+    </div>
+
   </div>
+
 </template>
+
+<style lang="stylus">
+	.box
+		padding 10px
+		margin 10px 20px
+		height 90px 
+		border 1px solid #333
+		border-radius 10px
+		a 
+			color #333
+			text-decoration none
+		p
+			line-height 30px
+	.tips 
+		margin 20px
+		color #999
+		font-size 14px
+		text-align justify
+		line-height 20px		
+				
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,13 @@
 	<div class="container">
 		<navbarSub></navbarSub>
 		<h2 class="text-center">
-			Home.vue
+			这里是 Home 页
 		</h2>
+		<div class="tips">
+			这一页呢，整个是个组件，然后在其中插入了顶栏的这个 navbarSub 组件，这个顶栏在 About 页和 More 页中也都用到了，中间的 title 文字是在用了路由配置里的 $route.name 信息，就是在 main.js 中配置路由时加入了 name 参数，然后进入该页时直接读取
+			<p>这边的 Back 是用了编程式导航 this.$router.go(-1);方法</p>
+			<p>点击 More 时呢，由于我在路由中加了 beforeEnter 方法，所以会弹出一个 confirm 框，这其实可以用作应用在跳转的提示啊，或者跳转前的一些动效方法等吧，而且除了 beforeEnter 还有其他的一些方法，具体可以看文档</p>
+		</div>
 	</div>
 </template>
 
